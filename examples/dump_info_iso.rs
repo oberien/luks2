89a89fc5ab444237bb36bf9d594d6198dfb26dcd
Max von Forell <max@vonforell.de>
2020-12-20 00:25:10 +0100

    add dump_info_iso example

---
+++ examples/dump_info_iso.rs
@@ -0,0 +1,27 @@
+//! Setup:
+//! 1. Create a file that will be encrytped via LUKS2:
+//!    `fallocate -l 1024M luks.iso`
+//! 2. Encrypt the file:
+//!    `cryptsetup luksFormat luks.iso`
+//! 3. Verify that everything worked:
+//!    `cryptsetup luksDump luks.iso`
+
+use luks2::{LuksHeader, LuksJson};
+use std::{fs::File, io::{Cursor, Read}};
+
+fn main() {
+	let mut f = File::open("D:\\Max\\Downloads\\tmp\\luks.iso").expect("could not open luks.iso");
+
+	let mut h = vec![0; 4096];
+	f.read_exact(&mut h).expect("could not read from luks.iso");
+	let header = LuksHeader::read_from(&mut Cursor::new(&h))
+		.expect("could not parse header");
+	println!("{}", header);
+
+	let mut j = vec![0; 3*4096];
+	f.read_exact(&mut j).expect("could not read from luks.iso");
+	let j: Vec<u8> = j.iter().map(|b| *b).filter(|b| *b != 0).collect();
+	let json = LuksJson::read_from(&mut Cursor::new(&j))
+		.expect("could not parse json");
+	println!("{:#?}", json);
+}
