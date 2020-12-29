6e7cf383838a25a8c8a33ede61a7bfb9d0fdb781
Max von Forell <max@vonforell.de>
2020-12-29 01:21:36 +0100

    remove iso files to not exceed crates.io size limit

--- examples/dump_info_iso.rs
+++ examples/dump_info_iso.rs
@@ -1,9 +1,13 @@
 use luks2::LuksDevice;
-use std::io::Cursor;
+use std::{fs::File, io::{Cursor, Read}};
 
 fn main() {
-	// example.iso was generated via `fallocate -l 1M example.iso && cryptsetup luksFormat example.iso` with the password "password"
-	let f = Cursor::new(include_bytes!("example.iso"));
+	// create example.iso via `fallocate -l 16M example.iso && cryptsetup luksFormat example.iso` with the password "password"
+	let path = format!("{}/examples/example.iso", env!("CARGO_MANIFEST_DIR"));
+	let mut f = File::open(path).expect("could not open example.iso; did you create it?");
+	let mut buf = vec![0; 16 * 1024 * 1024];
+	f.read_exact(&mut buf).expect("could not read from example.iso");
+	let f = Cursor::new(buf);
 
 	let luks_dev = LuksDevice::from_device(f, b"password", 512)
 		.expect("could not create device");
--- tests/mk_test.rs
+++ tests/mk_test.rs
@@ -1,9 +1,13 @@
 use luks2::LuksDevice;
-use std::io::Cursor;
+use std::{fs::File, io::{Cursor, Read}};
 
 #[test]
 fn mk_test() {
-    // test.iso was generated via `fallocate -l 1M test.iso && cryptsetup luksFormat test.iso` with the password "password"
-	let f = Cursor::new(include_bytes!("test.iso"));
+    // create test.iso via `fallocate -l 16M test.iso && cryptsetup luksFormat test.iso` with the password "password"
+	let path = format!("{}/tests/test.iso", env!("CARGO_MANIFEST_DIR"));
+	let mut f = File::open(path).expect("could not open test.iso; did you create it?");
+	let mut buf = vec![0; 16 * 1024 * 1024];
+	f.read_exact(&mut buf).expect("could not read from test.iso");
+	let f = Cursor::new(buf);
 	let _luks_device = LuksDevice::from_device(f, b"password", 512).unwrap();
 }
