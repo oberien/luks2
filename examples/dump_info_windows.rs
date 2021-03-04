7132d9ec797a75a3bef40dd17b41c1d2659749eb
Max von Forell <max@vonforell.de>
2021-03-04 22:27:35 +0100

    chore: update windows example to use BufferedHarddiskVolume instead of GPT

--- examples/dump_info_windows.rs
+++ examples/dump_info_windows.rs
@@ -1,23 +1,14 @@
-use gptman::GPT;
 use luks2::*;
-use windows_drives::BufferedPhysicalDrive;
+use windows_drives::BufferedHarddiskVolume;
 use std::io::{Read, Seek, SeekFrom};
 
 fn main() {
-	let drive_num = 0;
-	let mut drive = BufferedPhysicalDrive::open(drive_num)
-		.expect("could not open drive");
-
-	let gpt = GPT::find_from(&mut drive)
-		.expect("could not find GPT");
-	let partition = gpt.iter().nth(1)
-		.expect("could not get partition entry").1;
-
-	let partition = BufferedPhysicalDrive::open_bounded(
-		drive_num, (partition.starting_lba, partition.ending_lba)
+	let partition_num = 12;
+	let partition = BufferedHarddiskVolume::open(
+		partition_num
 	).expect("could not open partition");
 
-	println!("Enter password for partition: ");
+	println!("Enter password for partition:");
 	let password = password::read().expect("could not read password");
 
 	let sector_size = partition.geometry.bytes_per_sector;
